f336c7fa540ea9dc500fe12b85da2506d59a9efc
123mandakinisingh <mandakinisingh3735@gmail.com>
2022-05-04 02:28:39 -0700

    Firstcommit

---
+++ day1_task.py
@@ -0,0 +1,43 @@
+# first program 
+print("Hello everyone")
+
+#variables in python 
+
+
+
+
+
+#example1
+n=10
+string="Easy programming language"
+print(n)
+print(string)
+
+#example2
+a=10
+b=20
+print(a+b)
+
+#example3
+s1="hello"
+s2="world"
+print(s1+" "+s2)
+
+#example4
+x=y=z=50
+print(x)
+print(y)
+print(z)
+#example5
+p,q,r=10,30,70
+print(p)
+print(q)
+print(r)
+
+
+
+
+
+
+
+#DATA TYPES IN PYTHON
