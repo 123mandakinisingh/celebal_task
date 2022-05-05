864b4cf96016de696effe060fd38d4eb0e424a0d
123mandakinisingh <mandakinisingh3735@gmail.com>
2022-05-05 05:48:40 -0700

    secondcommit

---
+++ day2/task1.py
@@ -0,0 +1,39 @@
+#migratoryBirds
+
+
+
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the 'migratoryBirds' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+
+
+def migratoryBirds(arr):
+    # Write your code here
+    lis=[0]*len(arr)      
+    for i in arr:
+        lis[i]+=1
+    return lis.index(max(lis))
+                       
+                          
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
